0f76a418ae65ac4e32a06d41afd14b05949e3b45
romanopablo8 <romanopablo83@gmail.com>
2022-05-20 15:49:15 -0300

    dia 1 de mercado liebre

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const app = express();
+ const express = require('express');
+ const app = express();
 
-app.use('/static', express.static(__dirname + '/public'));
+ app.use ('/static', express.static(__dirname + '/public'));
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+ app.listen(3000, ()=>{
+   console.log('Servidor funcionando');
+ });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
+   res.sendFile(__dirname + '/views/home.html');
+ });
+
+
+
+// const express = require ('express');
+// const path = require ('path');
+// const app = express();
+// const port = 3000;
+//
+// app.get('/', (req, res) => {
+//  res.sendFile ( path.resolve ('./views/home.html'));
+//})
+//
+// app.listen(port, () => {
+//  console.log(`Example app listening on port ${port}`);
+// })
