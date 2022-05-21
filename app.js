6e7c117429d4252659760fbba24b07961e306040
romanopablo8 <romanopablo83@gmail.com>
2022-05-21 01:13:24 -0300

    act para desafio 2

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
  const express = require('express');
  const app = express();
 
- app.use ('/static', express.static(__dirname + '/public'));
+ app.use ('/static', express.static( __dirname + '/public'));
 
  app.listen(3000, ()=>{
    console.log('Servidor funcionando');
  });
 
 app.get('/', (req,res)=>{
-   res.sendFile(__dirname + '/views/home.html');
+   res.sendFile (__dirname + '/views/home.html');
  });
 
 
