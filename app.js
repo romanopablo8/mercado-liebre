47ce34fe39aa42ed33443643955f792bfa9173db
romanopablo8 <romanopablo83@gmail.com>
2022-06-10 22:25:56 -0300

    actualizacion pendiente

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
- const express = require('express');
- const app = express();
+const express = require('express');
+const app = express();
 
- app.use ('/static', express.static( __dirname + '/public'));
+app.use ('/static', express.static ( __dirname + '/public'));
 
- app.listen(3000, ()=>{
-   console.log('Servidor funcionando');
- });
+const port = process.env.PORT || 3000
 
-app.get('/', (req,res)=>{
-   res.sendFile ( __dirname + '/views/home.html');
- });
+app.listen(port, ()=>{
+  console.log('Servidor funcionando');
+});
 
- app.get('/', ( req, res)=>{
-  res.sendFile ( __dirname + '/views/register.html');
+app.get ('/', (req,res)=>{
+  res.sendFile (__dirname + '/views/home.html');
+});
+
+app.get ('/register', (req,res)=>{
+  res.sendFile (__dirname + '/views/register.html');
+});
+
+app.get ('/login', (req,res)=>{
+  res.sendFile (__dirname + '/views/login.html');
 });
 
 // const express = require ('express');
